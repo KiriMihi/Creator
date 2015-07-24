c235579b1b79bb49e04f416cb25b98aa5ff784ac
KiriMihi <kirimihi@rambler.ru>
2015-07-24 10:42:06 +0300

    Revert "1"
    This reverts commit 8346f90a27b8bf23155e48c0a6c0041c39c65b6e.

--- lego/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-	return render(request, 'bootstrap/allauth/account/index.html')
-
-def profile(request):
-	return render(request, 'bootstrap/allauth/account/profile.html')
